adaa21efccca2e21e94c08a3c34d54a768f08779
stanislavmg <stanislav.as1998@gmail.com>
2024-04-22 18:51:30 +0400

    completed init command and arguments

--- main.c
+++ main.c
@@ -1,24 +1,75 @@
-#include <stdio.h>
+#include "libft/libft.h"
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
 
-int main (int argc, char * argv[])
-{ 
-	int pipedes[2];
-	pid_t pid;
+typedef struct s_cmd{
+	char			*name;
+	char			**args;
+} t_list;
 
+t_list	*init_commands(int argc, char **argv);
+void	get_args(t_list *cmd, char *argv);
+
+int	main(int argc, char **argv, char **env)
+{
+	t_list	*arr;
+	int		pid;
+	int pipedes[2];
+
+	arr = init_commands(argc - 1, argv + 1);
+	if (!arr)
+		return (1);
+	
+	return (0);
+}
+
+t_list	*init_commands(int argc, char **argv)
+{
+	t_list	*rval;
+	int	i;
+
+	i = -1;
+	rval = (t_list *)malloc(argc * sizeof(t_list));
+	if (!rval)
+		return (NULL);
+	while (++i < argc)
+		get_args(rval + i, argv[i]);
+	return (rval);
+}
+
+void	get_args(t_list *cmd, char *argv)
+{
+	char	**tmp;
+	int		i;
+
+	tmp = ft_split(argv, ' ');
+	cmd->name = tmp[0];
+	cmd->args = tmp + 1;
+}
+
+void	rec_fork(int count, char **cmd, char **arg)
+{
+	int pipedes[2];
+	pid_t	pid;
+
+	if (count == -1)
+		return ;
 	pipe(pipedes);
 	pid = fork();
 	if ( pid == 0 ) 
 	{
+		rec_fork(count - 1, cmd + 1, arg + 1);
 		close(pipedes[0]);
 		dup2(pipedes[1], 1);
-		execve("/usr/sbin/ifconfig", NULL, NULL);
+		execve(cmd, arg, NULL);
 		close(pipedes[1]);
 	}
 	else
 	{
+		wait();
 		char buf[1024];
 		int len;
 		close(pipedes[1]);
@@ -26,5 +77,5 @@ int main (int argc, char * argv[])
 			write(2, buf, len);
 		close(pipedes[0]);
 	}
-	return 0;
+	return ;
 }
